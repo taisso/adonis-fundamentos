a79db591f12458c735cfd04f7dfcd38c42972ff3
taisso <taissoreni@gmail.com>
2021-08-24 17:22:45 -0300

    feat: create controller

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,28 @@
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import User from "App/Models/User";
+
+export default class UsersController {
+  public async index({ response }: HttpContextContract) {
+    const user = await User.all();
+
+    if (user) {
+      return response.notFound({ erros: { message: "Users not found" } });
+    }
+
+    return user;
+  }
+
+  public async store({ request, response }: HttpContextContract) {
+    const body = request.only(['email', 'password'])
+
+    const user = await User.create(body)
+
+    return user
+  }
+
+  public async show({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+}
---
+++ app/Models/User.ts
@@ -0,0 +1,19 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  email: string
+
+  @column()
+  password: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
